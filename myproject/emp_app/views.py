e2e4db54e93d65f5fadcb3a09f683e40aef19aea
AnJaLi21Patil <angalipatilsonu2003@gmail.com>
2025-11-20 14:39:37 +0530

    add JWT authentication for API endpoints

--- myproject/emp_app/views.py
+++ myproject/emp_app/views.py
@@ -114,7 +114,8 @@ def remove_emp(request, emp_id):
 def filter_emp(request):
     if "user" not in request.session:
         return redirect("login")
-    emps = []
+    # emps = []
+    emps= []
     if request.method == "POST":
         search = request.POST.get("name", "")
         try:
